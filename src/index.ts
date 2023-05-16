57e70ab4391e6b3347bcc79ecaf0ae49f1be858a
Jason Olson <jolson88@outlook.com>
2023-05-15 21:34:56 -0700

    Basic canvas framework

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-function delay(timeoutInMs: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, timeoutInMs));
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+
+function start(): void {
+  initializeCanvas();
 }
 
-async function main(): Promise<void> {
-  await delay(2000);
-  console.log("Hello after 2 seconds");
+function initializeCanvas(): void {
+  canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+  if (!canvas) {
+    throw new Error('Could not initialize canvas');
+  }
+
+  canvas.width = window.innerWidth * 2; // Account for high-DPI screens
+  canvas.height = window.innerHeight * 2;
+  canvas.style.width = `${window.innerWidth}px`;
+  canvas.style.height = `${window.innerHeight}px`;
+
+  context = canvas.getContext('2d') as CanvasRenderingContext2D;
+  if (!context) {
+    throw new Error('Could not get 2d context');
+  }
+
+  context.scale(2, 2);
+  context.fillStyle = 'rgb(0, 0, 255)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-console.log("Starting program...");
-main()
-  .then(() => console.log(`\n\nExiting program...`))
-  .catch(console.error);
+start();
